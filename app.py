20cfd7934a9086bb85dcb85db59f41d824d26a21
Swarup Sahoo <swarupsahoo@utexas.edu>
2018-09-25 17:14:16 -0500

    refresh app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import flask
-
-
-# In[ ]:
-
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+	return 'Hello, World!'
+
+if __name__ == '__main__':
+	app.run()
 
