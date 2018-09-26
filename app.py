fa3e499cbc0bbdbf402538c6c0a2e94ac73d6ce6
Swarup Sahoo <swarupsahoo@utexas.edu>
2018-09-26 11:58:00 -0500

    first functional translator

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pickle
 
-dictionary = pickle.load(open("dictionary.pkl","rb"))
+e2odictionary = pickle.load(open("dictionary.pkl","rb"))
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return render_template('index.html', translation = dictionary['type'])
+	return render_template('index.html')
+
+@app.route('/receiver',methods=['POST'])
+def translator():
+	data = request.get_json()
+	words = data.split(" ")
+	tr_words = []
+	for word in words:
+		if word in e2odictionary:
+			tr_words.append(e2odictionary[word])
+		else:
+			tr_words.append("**")
+	result = " ".join(tr_words)
+	return result
+
 
 if __name__ == '__main__':
 	app.run()
---
+++ static/e2oconvertor.py
@@ -0,0 +1,9 @@
+import sys
+
+text = sys.argv[1]
+
+def convert(text=text):
+  print(text)
+
+if __name__ == "__main__":
+  convert()
